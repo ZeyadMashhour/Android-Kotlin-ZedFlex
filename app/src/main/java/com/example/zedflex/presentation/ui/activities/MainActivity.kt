50713d573c104d82657a3c482c1cd8f7cb1bb2c3
ZeyadMashhour <91096543+ZeyadMashhour@users.noreply.github.com>
2023-08-30 19:38:07 +0300

    UI created
    Todo fix frgament navigation

--- app/src/main/java/com/example/zedflex/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/zedflex/presentation/ui/activities/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.zedflex.presentation.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toolbar
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.zedflex.R
+import com.example.zedflex.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
     }
+
+
 }
